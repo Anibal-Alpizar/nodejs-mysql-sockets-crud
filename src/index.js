48b47bb6f452637b13b0b801f926e9c5514c2e05
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-18 18:24:47 -0600

    sockets configuration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import express from 'express'
+import app from "./app.js";
+import { Server as Websocketserver } from "socket.io";
+import http from "http";
 
-console.log("Hello World");
+const server = http.createServer(app);
+const httpServer = server.listen(3000);
+const io = new Websocketserver(httpServer);
+
+console.log("Server is running on port 3000");
