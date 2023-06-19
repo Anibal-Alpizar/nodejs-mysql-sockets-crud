25df753fcff327a1b21a9d938ba58e46e3319a57
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-19 06:25:40 -0600

    websockets, client connection

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import app from "./app.js";
 import { Server as Websocketserver } from "socket.io";
 import http from "http";
 import { connectDB } from "./db.js";
+import sockets from "./sockets.js";
 
 connectDB();
 
 const server = http.createServer(app);
 const httpServer = server.listen(3000);
+console.log("Server is running on port 3000");
 const io = new Websocketserver(httpServer);
 
-console.log("Server is running on port 3000");
+sockets(io);
