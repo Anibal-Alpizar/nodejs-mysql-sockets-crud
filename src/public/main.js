dc626b1416f767fff3eb022b85e84c383fd495f3
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-30 20:19:39 -0600

    crud done

--- src/public/main.js
+++ src/public/main.js
@@ -1,3 +1,18 @@
-const socket = io();
+const noteForm = document.querySelector("#noteForm");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
 
-console.log(socket)
+noteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (savedId) {
+    updateNote(savedId, title.value, description.value);
+  } else {
+    saveNote(title.value, description.value);
+  }
+
+  title.value = "";
+  description.value = "";
+
+  title.focus();
+});
